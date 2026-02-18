ca1df1f273574584104f0d582258a54f5268c835
Elizar O. Dela Isla <elizarva20@gmail.com>
2026-02-19 02:03:02 +0800

    Update Function | Output and Return Values - Challenge.js

--- Function | Output and Return Values - Challenge.js
+++ Function | Output and Return Values - Challenge.js
@@ -10,3 +10,26 @@ Below 18.5	Underweight
 30 and above	Obesity
 
 */
+
+
+// BMI Calculator Challenge
+/*
+
+Create a BMI calculator using JavaScript functions. 
+  - The Body Mass Index (BMI) is a way of estimating the amount of body fat. It's used in medicine to calculate risk of heart disease.
+  - You can calculate it using the formula below, where weight divided by height squared.
+
+BMI = weight (kg) / height ^2 (m^2)
+
+Your challenge is to create a function that takes weight and height as inputs and gives the calculated BMI value as an output. 
+The output should be rounded to the nearest whole number.
+
+The first parameter should be the weight and the second should be the height.
+
+NOTE: 
+- You do not need to write any alerts or prompts or console logs. 
+- Your code should only contain the function, the result has to be returned by the function. You do not need to call the function.
+
+Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+
+*/
