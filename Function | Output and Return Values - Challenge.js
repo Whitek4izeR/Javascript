f70ecb8676bee7392d028e62a0867d019c913728
Elizar O. Dela Isla <elizarva20@gmail.com>
2026-02-19 01:20:35 +0800

    Create Function | Output and Return Values - Challenge.js

---
+++ Function | Output and Return Values - Challenge.js
@@ -0,0 +1,12 @@
+/*
+BMI - Body Mass Index
+BMI = weight (kg) / height ^2 (m^2)
+
+BMI Categories (Adults)
+BMI	Category
+Below 18.5	Underweight
+18.5 – 24.9	Normal weight
+25 – 29.9	Overweight
+30 and above	Obesity
+
+*/
