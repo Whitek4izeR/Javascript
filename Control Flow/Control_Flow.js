28dac998e5cc5abf1b587552f52bbb20cdc9de1b
Elizar O. Dela Isla <elizarva20@gmail.com>
2026-02-22 22:08:17 +0800

    Update and rename Random Number Generator.js to Control_Flow.js

--- Control Flow/Control_Flow.js
+++ Control Flow/Control_Flow.js
@@ -1,6 +1,13 @@
+// Random Number Generator
+
+
 prompt.("what is your name?");
 prompt.("what is their name?");
 
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 alert("Your love score is " + loveScore + "5");
+
+
+// If - Else Conditionals and Logic
+
