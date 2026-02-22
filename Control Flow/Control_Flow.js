bd1a20d9a7709b86ecb1a8a541cd572fe7988819
Elizar O. Dela Isla <elizarva20@gmail.com>
2026-02-22 22:14:49 +0800

    Update Control_Flow.js

--- Control Flow/Control_Flow.js
+++ Control Flow/Control_Flow.js
@@ -6,8 +6,20 @@ prompt.("what is their name?");
 
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
-alert("Your love score is " + loveScore + "5");
+alert("Your love score is " + loveScore + "%");
 
 
 // If - Else Conditionals and Logic
+// Same Code with If - Else
 
+prompt.("what is your name?");
+prompt.("what is their name?");
+
+var loveScore = Math.random() * 100;
+
+if (loveScore === 100){
+  alert("Your love score is " + loveScore + "%" + "You love each other like Kanye loves Kanye.");
+} else {
+  alert("Your love score is " + loveScore + "%");
+}
+loveScore = Math.floor(loveScore) + 1;
