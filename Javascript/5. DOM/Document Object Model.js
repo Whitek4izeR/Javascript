1058158f30c83594398e1f4272c95eeca433a97d
Elizar O. Dela Isla <elizarva20@gmail.com>
2026-03-13 22:53:31 +0800

    Update Document Object Model.js

--- Javascript/5. DOM/Document Object Model.js
+++ Javascript/5. DOM/Document Object Model.js
@@ -1,1 +1,15 @@
-//Enter here
+//Best to place the scrpt at the bottom so it will load the html and css first before executing the script
+// If you place the script on top it will find the objects of the html which is not yet present or loaded > this will cause an error.
+
+<html>
+<head>
+<title>The new Website</title>
+    <link rel="stylesheet" href="styles.css">
+        
+</head>
+    <body>
+        <h1>Hello</h1>
+
+    <script src="index.js" charset="utf-8"></script>
+    </body>
+</html>
